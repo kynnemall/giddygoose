7b3f5fa5f6fc3f22f10040043c343203dfa9c9e7
martin <sideproject1892@gmail.com>
2024-01-18 19:57:57 +0000

    Homepage and styling

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(page_title="Giddy Goose", layout="centered")
+
+st.header('Just Giddy Goose things')
+st.markdown("""
+    This is the app.<br>It does things<br>Choose a page on the left and see what happens
+""", unsafe_allow_html=True)
