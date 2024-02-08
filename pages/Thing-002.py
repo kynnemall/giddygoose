27d31609ae4cabd0f8fbef6120784b6f1d308f49
martin <sideproject1892@gmail.com>
2024-02-08 22:57:16 +0000

    Experimented with models predicting 4 classes

--- pages/Thing-002.py
+++ pages/Thing-002.py
@@ -8,6 +8,7 @@ Created on Wed Feb  7 20:27:57 2024
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import streamlit as st
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
@@ -55,45 +56,60 @@ def prepare_data(df):
 
 
 def dtree_decision_boundary(cols, dtree=False):
-    plot_colors = "yrb"
+    plot_colors = "wrby"
     classes = data['class'].unique()
     X = data[cols].values
     y = data['class'].values
+    global clf
     if dtree:
         clf = DecisionTreeClassifier(
             random_state=42, class_weight='balanced', min_impurity_decrease=5e-2
         )
     else:
-        clf = LinearDiscriminantAnalysis()
+        clf = LinearDiscriminantAnalysis(n_components=2)
 
     clf.fit(X, y)
-
-    fig, ax = plt.subplots()
-    DecisionBoundaryDisplay.from_estimator(
-        clf, X, cmap=plt.cm.RdYlBu, response_method="predict",
-        xlabel=cols[0], ylabel=cols[1], ax=ax
-    )
-    for i, color in zip(classes, plot_colors):
-        idx = np.where(y == i)
-        plt.scatter(
-            X[idx, 0],
-            X[idx, 1],
-            c=color,
-            label=i,
-            cmap=plt.cm.RdYlBu,
-            edgecolor="black",
-            s=30,
-        )
     score = clf.score(X, y)
-    plt.title(f'Classifier score: {score:.1%}')
-    plt.legend()
-    plt.tight_layout()
+
+    if len(cols) == 2:
+        fig, ax = plt.subplots()
+        DecisionBoundaryDisplay.from_estimator(
+            clf, X, cmap=plt.cm.RdYlBu, response_method="predict",
+            xlabel=cols[0], ylabel=cols[1], ax=ax
+        )
+        for i, color in zip(classes, plot_colors):
+            idx = np.where(y == i)
+            plt.scatter(
+                X[idx, 0],
+                X[idx, 1],
+                c=color,
+                label=i,
+                cmap=plt.cm.RdYlBu,
+                edgecolor="black",
+                s=30,
+            )
+        plt.title(f'Classifier score: {score:.1%}')
+        plt.legend()
+        plt.tight_layout()
+
+    if isinstance(clf, LinearDiscriminantAnalysis):
+        comps = clf.transform(X)
+        comps = pd.DataFrame(comps, columns=['LDA 1', 'LDA 2'])
+        comps['Class'] = y
+        plt.figure()
+        sns.scatterplot(x='LDA 1', y='LDA 2', data=comps, hue='Class')
+        sns.despine()
+        plt.tight_layout()
+
     return score
 
 
 # this feature combo gives the best accuracy
-dtree_decision_boundary(['fat:flour ratio', 'sugar'])
+dtree_decision_boundary(['fat:flour ratio', 'flour', 'sugar'], False)
 
+# ['fat:flour ratio', 'flour', 'sugar'] for LDA with 3 features: 88%
+# ['fat:flour ratio', 'sugar'] for LDA with 2 features: 84.5%
+# ['flour:sugar ratio', 'flour', 'fat'] for DT with 3 features: 90%
 
 # %%
 df = load_data()
